53f8a8eee12a73cdf59c4ea7041a77d06c6e52b4
Mateuszvip60 <147926693+Mateuszvip60@users.noreply.github.com>
2024-10-25 10:46:13 +0200

    script.js

--- script.js
+++ script.js
@@ -46,7 +46,3 @@ function startGeneratingPapieze() {
         }
 
         // Rozpocznij animację dla nowego papieża
-        requestAnimationFrame(movePapiez);
-
-    }, 10); // Papież generuje się co 10ms
-}
